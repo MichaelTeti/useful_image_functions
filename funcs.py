0d347f7207d3fa80094a9c9d52bc82174feb476e
Michael A. Teti <mtetifau@gmail.com>
2016-12-01 08:20:23 -0500

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -33,3 +33,19 @@ def read_image_files(im_height, im_width):
     if cv2.waitKey(10)==27:
       break
   return all_files
+
+def zca_whitening_matrix(X):
+    ''' whitens data in form of m x n, where the rows are the features of each example
+        and the columns are different examples.'''
+    # Covariance matrix [column-wise variables]: Sigma = (X-mu)' * (X-mu) / N
+    sigma = np.cov(X, rowvar=True) # [M x M]
+    # Singular Value Decomposition. X = U * np.diag(S) * V
+    U,S,V = np.linalg.svd(sigma)
+        # U: [M x M] eigenvectors of sigma.
+        # S: [M x 1] eigenvalues of sigma.
+        # V: [M x M] transpose of U
+    # Whitening constant: prevents division by zero
+    epsilon = 1e-5
+    # ZCA Whitening matrix: U * Lambda * U'
+    ZCAMatrix = np.dot(U, np.dot(np.diag(1.0/np.sqrt(S + epsilon)), U.T)) # [M x M]
+    return np.dot(ZCAMatrix, X)
