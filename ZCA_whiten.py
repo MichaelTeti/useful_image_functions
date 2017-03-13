c964a33085d1ead04d6e886669ed7aa9b4232b1a
Michael A. Teti <mtetifau@gmail.com>
2017-03-12 23:11:12 -0400

    Whitening

---
+++ ZCA_whiten.py
@@ -0,0 +1,18 @@
+import cv2
+import os
+
+def zca_whitening_matrix(X):
+    ''' whitens data in form of m x n, where the rows are the features of each example
+        and the columns are different examples.'''
+    # Covariance matrix [column-wise variables]: Sigma = (X-mu)' * (X-mu) / N
+    sigma = np.cov(X, rowvar=True) # [M x M]
+    # Singular Value Decomposition. X = U * np.diag(S) * V
+    U,S,V = np.linalg.svd(sigma)
+        # U: [M x M] eigenvectors of sigma.
+        # S: [M x 1] eigenvalues of sigma.
+        # V: [M x M] transpose of U
+    # Whitening constant: prevents division by zero
+    epsilon = 1e-5
+    # ZCA Whitening matrix: U * Lambda * U'
+    ZCAMatrix = np.dot(U, np.dot(np.diag(1.0/np.sqrt(S + epsilon)), U.T)) # [M x M]
+    return np.dot(ZCAMatrix, X)
