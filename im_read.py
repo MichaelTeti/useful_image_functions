53a12d1742e2c3c1499ddef7c62014e5ff4dedd1
Michael A. Teti <mtetifau@gmail.com>
2018-02-02 16:34:11 -0500

    Update im_read.py

--- im_read.py
+++ im_read.py
@@ -16,23 +16,27 @@ def img_reader(dir, imsz, file_ex=None):
         file_ex = '*'
     else:
         file_ex = '*' + file_ex
+
     folders = os.listdir(dir)
     imgs = np.zeros([0, imsz[0], imsz[1], 3])
-    labels = []
+    labels = np.zeros([0, 1])
     os.chdir(dir)
 
     for folder in xrange(len(folders)):
-        if os.path.isdir(folders[folder]):
-            os.chdir(dir + '/' + folders[folder])
-            files = glob.glob(file_ex)            
+        os.chdir(dir + '/' + folders[folder])
+        files = glob.glob(file_ex)
+        imgs2 = np.zeros([len(files), imsz[0], imsz[1], 3])
+        i = 0
 
-            for file in files:
-                if os.path.isfile(file):
-                    img = imread(file)
-                    img = imresize(img, [imsz[0], imsz[1]])
-                    imgs = np.concatenate((imgs, img), 0)
-                    labels.append(folder)
+        for file in files:
+            if os.path.isfile(file):
+                img = imread(file)
+                img = imresize(img, [imsz[0], imsz[1]])
+                imgs2[i, ...] = img
+                i += 1
+                
+        imgs = np.concatenate((imgs, imgs2), 0)
+        labels_new = np.ones([1, 1]) * folder
+        labels = np.concatenate((labels, labels_new), 0)
 
     return imgs, labels
-
-
