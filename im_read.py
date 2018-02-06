dbda2dda50d45d3aa736b31199b5678f7f6b8ec8
Michael A. Teti <mtetifau@gmail.com>
2018-02-06 09:22:06 -0500

    Update im_read.py

--- im_read.py
+++ im_read.py
@@ -5,13 +5,28 @@ from scipy.misc import imread, imresize
 
 
 def img_reader(dir, imsz, file_ex=None):
-    ''' A function to read images in folders
-        located in dir, resize them to 
-        imsz[0] x imsz[1]. If file_ex is 
-        given, it will only compile files of
-        type file_ex. Returns the images in 
-        NHWC format and the labels. '''
-
+    ''' A function that reads in images contained
+        in each folder in dir, resizes them to 
+        imsz[0] x imsz[1], and attaches a label to 
+        each image based on the folder it came from. 
+        
+        Args:
+             dir: directory where the image folders are 
+                  located.
+             imsz: a list of length 2, where each number 
+                   represents the height and width each
+                   image will be resized to. If all images 
+                   are the same size and don't need to be
+                   resized, just put the size they already 
+                   are.
+              file_ex: if the images you want to compile 
+                       are all of the same extension and/or
+                       there are other file types in the 
+                       folders that you don't want to read,
+                       you can include the extension as file_ex.
+                       Example, file_ex = '.jpg'
+    ''' 
+    
     if file_ex is None:
         file_ex = '*'
     else:
